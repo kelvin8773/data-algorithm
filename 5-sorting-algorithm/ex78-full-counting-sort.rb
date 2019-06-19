d97a98dee2a9bd319882914cf527c150ead789f3
Kelvin Liang <kelvin9877@gmail.com>
2019-06-19 17:17:15 +0800

    finish full counting sort

---
+++ 5-sorting-algorithm/ex78-full-counting-sort.rb
@@ -0,0 +1,10 @@
+def full_counting_sort(array)
+  result = Array.new(array.size, [])
+  array.map do |x|
+    result[x.to_i] += x.scan(/[A-Za-z]+/)
+  end
+  result.flatten
+end
+
+p full_counting_sort(["0 ab", "6 cd", "0 ef", "6 gh", "4 ij", "0 ab", "6 cd", "0 ef", "6 gh", "0 ij", "4 that", "3 be", "0 to", "1 be", "5 question", "1 or", "2 not", "4 is", "2 to", "4 the"])
+# => ["ab", "ef", "ab", "ef", "ij", "to", "be", "or", "not", "to", "be", "ij", "that", "is", "the", "question", "cd", "gh", "cd", "gh"]
