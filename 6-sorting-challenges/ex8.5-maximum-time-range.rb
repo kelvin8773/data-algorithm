754b1daa93d0af9b2ce73114930d45224f669169
Kelvin Liang <kelvin9877@gmail.com>
2019-07-02 18:12:31 +0800

    finishing smallest difference

--- 6-sorting-challenges/ex8.5-maximum-time-range.rb
+++ 6-sorting-challenges/ex8.5-maximum-time-range.rb
@@ -13,6 +13,20 @@ def advanced_quicksort(array, start=0, last=array.size-1)
   advanced_quicksort(array, middle+1, last)
 end
 
+def merge_sort(array1, array2)
+  index = 0
+  until array2.size < 1 
+    array2[0].first < array1[index].first ? array1.insert(index, array2.shift) 
+                  : index >= array1.size-1 ? (array1 = array1 + array2; break) : index += 1
+  end
+  return array1  
+end
+
+def full_merge_sort(array)
+    return array if array.size <= 1
+    merge_sort(full_merge_sort(array[0...array.size/2]), full_merge_sort(array[array.size/2..-1]))
+end
+
 def get_max_overlap(events,index=0)  
 
   first = events[0].first 
@@ -36,54 +50,139 @@ def maximum_time_range_k(array)
   return result
 end
 
+
+def maximum_time_range_k_2(array)
+  events = array.each_slice(2).to_a
+  full_merge_sort(events)
+  result, index = [], 0
+
+  while index < events.size-1
+    first, last, move = get_max_overlap(events[index..-1])
+    index += move + 1
+    result << [first, last]
+  end
+  result.flatten! if result.size < 2
+  return result
+end
+
+def maximum_time_range_k_3(array)
+  events = array.each_slice(2).to_a
+  events = events.sort
+  result, index = [], 0
+
+  while index < events.size-1
+    first, last, move = get_max_overlap(events[index..-1])
+    index += move + 1
+    result << [first, last]
+  end
+  result.flatten! if result.size < 2
+  return result
+end
+
 ################
 
 def maximum_time_range_y(array)
-  array_ar, i = [], 0
-  while i < array.length-1
-   array_ar << [array[i], array[i+1]]
-   i += 2
-  end
-  advanced_quicksort_y(array_ar);  ranges(array_ar)
-end 
-
-def advanced_quicksort_y(array, ind1=0, ind2=array.size-1)
-  return if ind2 - ind1 < 1
-  pivot = array[ind2].first
-  change = ind1
-  for ind in ind1..ind2
-    if array[ind].first <= pivot
-      array[change], array[ind] = array[ind], array[change]
-      change += 1
-    end
-  end
-  advanced_quicksort(array, ind1, change-2)
-  advanced_quicksort(array, change, ind2)
-end
-
-def ranges(array)
-  ind = 0
-  j = 1
-  res = []
-  while j < array.size
-    if array[j].first >= array[ind].last
-      res << array[ind]
-      ind = j
-    else
-      temp = array[ind].first
-      until j >= array.size || array[j].first >= array[ind].last
-        ind = j
-        j += 1
+  array = array.each_slice(2).to_a
+  advanced_quicksort_y(array)
+  ranges(array)
+  end 
+  
+  def advanced_quicksort_y(array, ind1=0, ind2=array.size-1)
+    return if ind2 - ind1 < 1
+    pivot = array[ind2].first
+    change = ind1
+    for ind in ind1..ind2
+      if array[ind].first <= pivot
+        array[change], array[ind] = array[ind], array[change]
+        change += 1
       end
-      j = array.size-1 if j >= array.size
-      res << [temp, array[j].last]
-      ind = j
     end
-    j += 1
+    advanced_quicksort_y(array, ind1, change-2)
+    advanced_quicksort_y(array, change, ind2)
   end
-  res.flatten! if res.length == 1
-  res
+  
+  def ranges(array)
+    ind = 0
+    j = 1
+    res = []
+    while j < array.size
+      if array[j].first > array[ind].last
+        res << array[ind]
+        ind = j
+      else
+        temp = array[ind].first
+        max = array[ind].last
+        until j >= array.size || array[j].first > array[ind].last
+          ind = j
+          j += 1
+          max = array[ind].last if max < array[ind].last
+        end
+           if j >= array.size-1
+             ind = array.size-1
+             max = array[ind].last if max < array[ind].last
+           end
+           res << [temp, max]
+           ind = j
+      end
+      j += 1
+    end
+    res.flatten! if res.length == 1
+    res
+  end
+
+##############
+
+def maximum_time_range_e(array)
+  get_overlaps(sort_time_slots(array))
 end
+
+def get_overlaps(array)
+  current_max = array.first
+  result = []
+  array[1..-1].each do |this_event|
+    if this_event.first < current_max.last
+      current_max = (current_max + this_event).minmax
+    else
+      result << current_max
+      current_max = this_event
+    end
+  end
+  result << current_max
+  return result.first if result.size == 1
+  result
+end
+
+def sort_time_slots(array)
+  return [array] if array.length == 2
+  left, right = partiton(array)
+  merge_arrays(sort_time_slots(left), sort_time_slots(right))
+end
+
+def partiton(array)
+  mid_point = array.length / 2
+  mid_point += 1 if mid_point.odd?
+  [array[0...mid_point], array[mid_point..-1]]
+end
+
+def merge_arrays(array1, array2)
+  sorted = []
+  until array1.empty? || array2.empty?
+    if array1.first.first < array2.first.first
+      sorted << array1.shift
+    else
+      sorted << array2.shift
+    end
+  end
+  sorted << array2.shift until array2.empty?
+  sorted << array1.shift until array1.empty?
+  
+  sorted
+end
+
+
+
+
+
 
 
 # p maximum_time_range([10, 18, 4, 6, 14, 16, 5, 8])
@@ -92,15 +191,27 @@ end
 # p maximum_time_range([5, 14, 1, 4, 13, 19, 3, 6, 9, 8])
 # => [1, 19]
 
-# p maximum_time_range([0, 1, 14, 16, 4, 8, 10, 12, 7, 9, 8, 15, 3, 5])
+# p maximum_time_range_y([0, 1, 14, 16, 4, 8, 10, 12, 7, 9, 8, 15, 3, 5])
 # => [[0,1], [3,16]]
 
-list = [0, 1, 14, 16, 4, 8, 10, 12, 7, 9, 8, 15, 3, 5]
+list1 = [10, 18, 4, 6, 14, 16, 5, 8]
+list2 = [5, 14, 1, 4, 13, 19, 3, 6, 9, 8]
+list3 = [0, 1, 14, 16, 4, 8, 10, 12, 7, 9, 8, 15, 3, 5]
 
-Benchmark.ips do |x|
-  x.config(:time => 5, :warmup => 3)
-  
-  x.report("Kelvin's method ") {maximum_time_range_k(list)}
-  x.report("Yunus's method ") {maximum_time_range_y(list)}
-  x.compare!    
+all_list = [list1, list2, list3]
+
+all_list.each do |list|
+p "Benchmark test with #{list}"
+    Benchmark.ips do |x|
+      x.config(:time => 3, :warmup => 2)
+      
+      x.report("Kelvin's solution (advance Sort) ") {maximum_time_range_k(list)}
+      x.report("Kelvin's solution (merge Sort) ") {maximum_time_range_k_2(list)}
+      x.report("Kelvin's solution (Ruby Sort) ") {maximum_time_range_k_3(list)}
+      x.report("Yunus's  solution ") {maximum_time_range_y(list)}
+      x.report("Ebuka's  solution ") {maximum_time_range_e(list)}
+      x.compare!    
+    end
 end
+
+# p maximum_time_range_k_3(list1)
---
+++ 6-sorting-challenges/ex8.6-smallest-difference.rb
@@ -0,0 +1,161 @@
+require 'benchmark/ips'
+
+def advanced_quicksort(array, start=0, last=array.size-1)
+  return array if last-start < 1
+  pivot = array[last]
+  middle = start - 1
+
+  for i in start..last
+   array[i] <= pivot ? (middle += 1; array[i], array[middle] = array[middle], array[i] ) : array[i]
+  end
+
+  advanced_quicksort(array, start, middle-1)
+  advanced_quicksort(array, middle+1, last)
+end
+
+def smallest_difference(array)
+  advanced_quicksort(array)
+  differences = []
+
+  for i in 0..array.size-2
+    differences << array[i+1] - array[i]
+  end
+
+  index = differences.index(differences.min)
+  [array[index], array[index+1]]
+
+end
+
+def smallest_difference_1(array)
+  array = array.sort
+  differences = []
+
+  for i in 0..array.size-2
+    differences << array[i+1] - array[i]
+  end
+
+  index = differences.index(differences.min)
+  [array[index], array[index+1]]
+
+end
+
+def smallest_difference_m(array)
+  numbers = [array[0], array[1]]
+  min_dif = (array[0] - array[1]).abs
+  
+  array.each_with_index do |n1, i|
+    array[(i + 1)..-1].each do |n2|
+      diff = (n1 - n2).abs
+      
+      if min_dif > diff
+        numbers = [n1, n2]
+        min_dif = diff
+      end
+    end
+  end
+  
+  [numbers.min, numbers.max]
+end
+
+#######################
+
+# Yunus's Solution
+
+def smallest_difference_y(array)
+  array = recursive_merge(array)
+  min = array[1] - array[0]
+  start, endy = 0, 1
+  array.each_with_index do |num, ind|
+    if ind+1 < array.size && array[ind+1] - num < min
+      min = array[ind+1] - num
+      start, endy = ind, ind+1
+    end
+    
+  end
+  [array[start], array[endy]]
+end
+
+def recursive_merge(array)
+  return array if array.size == 1
+  half = (array.length-1)/2
+  array1, array2 = array[0..half], array[half+1..-1]
+  merge(recursive_merge(array1), recursive_merge(array2))
+end
+
+def merge(array1, array2)
+   a1, a2 = 0, 0
+ res = []
+ until a1 == array1.size || a2 == array2.size
+ if array1[a1] <= array2[a2]
+   res << array1[a1]
+   a1 += 1
+ else
+   res << array2[a2]
+  a2 += 1
+ end
+ end
+ if a1 < array1.size
+   res + array1[a1..-1]
+ elsif a2 < array2.size
+ res + array2[a2..-1]
+ else
+   res
+  end
+end
+
+##################
+# Afani's Solution
+
+def smallest_difference_a(array)
+  # write your code here
+  infinite_diff = Float::INFINITY
+  min_nums = []
+  arr = array.sort
+  for i in 0...arr.length-1
+    if (arr[i+1] - arr[i]) < infinite_diff
+      infinite_diff = (arr[i+1] - arr[i])
+      min_nums = [arr[i+1], arr[i]]
+    end
+  end
+  min_nums.minmax
+  
+end
+
+##################
+
+
+
+
+# p smallest_difference([-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854])
+# => [-20, 30]
+
+# p smallest_difference([1000, -3916237, -357920, -3620601, 7374819, -7330761, 1003, 6246457, -6461594, 266854])
+# => [1000, 1003]
+
+# p smallest_difference([-5935536, -4857144, 8762611, -3710156, -62196, 5726532, 3880783, 8228893, 4557632, 8243459, 8058689, -2004515, -2532128, -2809277, 6230123, -1534444, 7594023, 8030804, -808395, 3895414, -1042916, -6349277, 8056932, 6449041, -5776338, -5663713, -2118668, -7889996, -7194680, 4072724, 5817742, 52723, -9978630, -4565272, -905845, 2413011, 6580118, 7342503, 9920829, 3264328, -2498614, -7173602, 9550646, -3508831, 6660598, 7160307, -1454798, -7810912, -6934054, 5412837, -3256366, 5648256, 7539528, 8948739, -9432879, 540367, -683895, -4837582, 404577, -7427248, 7493712, -7517462, 59838, 7493355, 2624635, 7528881, 4482783, 5371130, -7137887, 5776578, -4043963, -4861937, -5490722, -3065192, -1330264, -5049584, 8815653, -2956679, -3663308, 3294818, -3792431, -6884969, -6327570, -3304021, -7320937, -2754952, 5645257, 1616120, 2854718, 3721762, 9874605, 5629627, 5448934, 6511446, -4557691, 4035783, -4467467, 2150627, 6427050, 3916593])
+# => [7493355, 7493712]
+
+#################################
+
+
+list1 = [-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854]
+list2 = [1000, -3916237, -357920, -3620601, 7374819, -7330761, 1003, 6246457, -6461594, 266854]
+list3 = [-5935536, -4857144, 8762611, -3710156, -62196, 5726532, 3880783, 8228893, 4557632, 8243459, 8058689, -2004515, -2532128, -2809277, 6230123, -1534444, 7594023, 8030804, -808395, 3895414, -1042916, -6349277, 8056932, 6449041, -5776338, -5663713, -2118668, -7889996, -7194680, 4072724, 5817742, 52723, -9978630, -4565272, -905845, 2413011, 6580118, 7342503, 9920829, 3264328, -2498614, -7173602, 9550646, -3508831, 6660598, 7160307, -1454798, -7810912, -6934054, 5412837, -3256366, 5648256, 7539528, 8948739, -9432879, 540367, -683895, -4837582, 404577, -7427248, 7493712, -7517462, 59838, 7493355, 2624635, 7528881, 4482783, 5371130, -7137887, 5776578, -4043963, -4861937, -5490722, -3065192, -1330264, -5049584, 8815653, -2956679, -3663308, 3294818, -3792431, -6884969, -6327570, -3304021, -7320937, -2754952, 5645257, 1616120, 2854718, 3721762, 9874605, 5629627, 5448934, 6511446, -4557691, 4035783, -4467467, 2150627, 6427050, 3916593]
+
+
+all_list = [list1, list2, list3]
+
+all_list.each do |list|
+# p "Benchmark test with #{list}"
+    Benchmark.ips do |x|
+      x.config(:time => 3, :warmup => 2)
+      
+      x.report("Kelvin-Adv Q-Sort") {smallest_difference(list)}
+      x.report("Kelvin-Ruby Sort") {smallest_difference_1(list)}
+      x.report("Model-No Sort") {smallest_difference_m(list)}
+      x.report("Yunus-Merge Sort") {smallest_difference_y(list)}
+      x.report("Afani-Ruby Sort") {smallest_difference_a(list)}
+
+      x.compare!    
+    end
+end
