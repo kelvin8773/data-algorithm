60e4bd497e644af0983ad792f4e2ec4e31ab8403
Kelvin Liang <kelvin9877@gmail.com>
2019-08-22 16:56:37 +0800

    finishing sort_colors

---
+++ 8-leetcode/075-sort_colors.rb
@@ -0,0 +1,23 @@
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+
+def sort_colors(nums)
+  counts = Array.new(3, 0)
+  nums.each {|x| counts[x] += 1 }
+
+  idx = 0
+
+  counts.each_with_index do |num, i|
+    num.times do 
+      nums[idx] = i
+      idx += 1
+    end
+  end
+
+  nums
+
+end
+
+# p sort_colors([2,0,2,1,1,0])
+
+p sort_colors([2,0,2,1,1,0,1,0,2,1])
